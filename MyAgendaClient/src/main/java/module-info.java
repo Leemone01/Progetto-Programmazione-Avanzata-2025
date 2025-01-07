2ea995b90d4b5d2ecc32d8924b82474e4503a64a
Simone Calò <83040047+Leemone01@users.noreply.github.com>
2025-01-07 17:20:24 +0100

    Delete MyAgendaClient directory

--- MyAgendaClient/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module it.unipi.myagendaclient {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-    requires com.google.gson;
-
-    opens it.unipi.myagendaclient to javafx.fxml;
-    exports it.unipi.myagendaclient;
-}
